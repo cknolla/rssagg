d76b22a73afb8b242d64332f816c2d8a49e5f024
Casey Knolla <cknolla@gmail.com>
2024-06-23 15:31:06 -0700

    couple working endpoints

---
+++ json.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("failed to marshal json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errResponse{Error: message})
+}
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatalln("PORT environment variable not set")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	v1router := chi.NewRouter()
+	v1router.Get("/healthz", handlerReadiness)
+	v1router.Get("/err", handlerErr)
+	router.Mount("/v1", v1router)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server starting on port %s", portString)
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
